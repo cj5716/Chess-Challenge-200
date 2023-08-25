63ae099cb5c1f145c61940e89e726e5d72c64c8f
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-25 13:30:34 +0800

    don't trust incomplete search, 272 tokens

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿using System; // #DEBUG
 using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    Move bestMove = Move.NullMove;
     int[] material = {0, 159, 450, 434, 716, 1421, 0};
 
     private int Evaluate(Board board)
     {
         int score = 0;
-        for (var pieceIndex = 0; ++pieceIndex <= 6;)
+        for (int pieceIndex = 0; ++pieceIndex <= 6;)
             score += material[pieceIndex] * 
                     (BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard((PieceType)pieceIndex, true))
                    - BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard((PieceType)pieceIndex, false)));
         return board.IsWhiteToMove ? score : -score;
     }
 
-    private int Search(Board board, Timer timer, int allocatedTime, int ply, int depth)
+    private int Search(Board board, Timer timer, int allocatedTime, int ply, int depth, out Move bestMove)
     {
+        bestMove = Move.NullMove;
+
         if (ply > 0 && board.IsRepeatedPosition())
             return 0;
 
@@ -30,20 +31,20 @@ public class MyBot : IChessBot
 
         int bestScore = -20000;
 
-        foreach (var move in moves)
+        foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int score = -Search(board, timer, allocatedTime, ply + 1, depth - 1);
+            int score = -Search(board, timer, allocatedTime, ply + 1, depth - 1, out _);
             board.UndoMove(move);
-
-            if (depth > 1 && timer.MillisecondsElapsedThisTurn > allocatedTime)
-                return bestScore;
 
             if (score > bestScore)
             {
                 bestScore = score;
-                if (ply == 0) bestMove = move;
+                bestMove = move;
             }
+
+            if (depth > 1 && timer.MillisecondsElapsedThisTurn > allocatedTime)
+                return bestScore;
         }
 
         return bestScore;
@@ -51,14 +52,18 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        bestMove = Move.NullMove;
-        var allocatedTime = timer.MillisecondsRemaining / 30;
+        Move bestMove = Move.NullMove;
+        int allocatedTime = timer.MillisecondsRemaining / 30;
 
-        for (var depth = 1; timer.MillisecondsElapsedThisTurn <= allocatedTime; depth++)
-        { // #DEBUG
-            Search(board, timer, allocatedTime, 0, depth);
+        for (int depth = 1;;depth++)
+        {
+            Search(board, timer, allocatedTime, 0, depth, out var move);
+
+            if (timer.MillisecondsElapsedThisTurn > allocatedTime) break;
+
+            bestMove = move;
             Console.WriteLine($"depth {depth} {bestMove}"); // #DEBUG
-        } // #DEBUG
+        }
 
         return bestMove;
     }
